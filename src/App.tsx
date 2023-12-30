d44f1d27ad0330633e387dfae3922406952b173b
huanNguyen2204 <huan.nc2204@sinhvien.hoasen.edu.vn>
2023-12-30 22:32:03 +0700

    adding react-jsx default

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Reactjs core</h1>
+      <p>Lets enjoy the core and this game.</p>
+    </div>
+  )
+}
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,80 @@
+import path from 'path';
+// import fs from "fs";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ESLintWebpackPlugin from 'eslint-webpack-plugin';
+import Dotenv from 'dotenv-webpack';
+import { Configuration as WebpackConfig, HotModuleReplacementPlugin } from 'webpack';
+import { Configuration as WebpackDevServerConfig } from 'webpack-dev-server';
+
+type Configuration = WebpackConfig & {
+  devServer?: WebpackDevServerConfig;
+};
+
+const config: Configuration = {
+  mode: 'development',
+  output: {
+    publicPath: '/',
+  },
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      /* -- JS/TS loader -- */
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
+
+      /* -- CSS loader --*/
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.less$/i,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+
+      /* -- File assets -- */
+      {
+        test: /\.(png|jpg|gif|jpeg)$/i,
+        type: 'asset/resource'
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.jsx', '.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'public/index.html' }),
+    new Dotenv(),
+    new HotModuleReplacementPlugin(),
+    new ESLintWebpackPlugin(),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    port: 8888,
+    open: true,
+    hot: true,
+    // https: {
+    //   key: fs.readFileSync('cert/key.pem'),
+    //   cert: fs.readFileSync('cert/cert.pem'),
+    // }
+  },
+};
+
+export default config;
