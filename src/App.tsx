9dd1b5672cf491891eb904d2697165e0f8a32d76
Nguyễn Cao Huân <149618411+huanNguyen2204@users.noreply.github.com>
2025-04-27 01:39:27 +0700

    change name input

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.scss";
+import "./app.scss";
 
 const App = () => {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "@/App";
+import App from "@/app";
 import "./index.css";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
